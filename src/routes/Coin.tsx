1439b78cff26a1fb7934348c40d4a988b373c71d
narin <nalynnshinhwa1128@gmail.com>
2023-03-24 22:40:35 +0900

    nested route 사용해 부분 렌더링하기, useRouteMatch 훅을 사용해 상태 관리하기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Link, Route, Switch, useLocation, useParams, useRouteMatch } from "react-router-dom";
 import styled from "styled-components";
+import Chart from "./Chart";
+import Price from "./Price";
 
 interface RouteParams {
     coinId: string;
@@ -25,6 +27,52 @@ const Title = styled.h1`
 `;
 const Loader = styled.h1`
     text-align: center;
+`;
+
+const Overview = styled.div`
+    display: flex;
+    justify-content: space-between;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 10px 20px;
+    border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    span: first-child {
+        font-size: 10px;
+        font-weight: 400;
+        text-transform: uppercase;
+        margin-bottom: 5px;
+    }
+`;
+
+const Description = styled.p`
+    margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin: 25px 0px;
+    gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: 400;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 7px 0px;
+    border-radius: 10px;
+    color: ${props => (props.isActive ? props.theme.accentColor : props.theme.textColor)};
+    a {
+        display: block;
+    }
 `;
 
 interface RouteState {
@@ -119,14 +167,18 @@ function Coin() {
     const [info, setInfo] = useState<IInfoData>();
     const [priceInfo, setPriceInfo] = useState<IPriceData>();
 
+    const priceMatch = useRouteMatch("/:coinId/price");
+    const chartMatch = useRouteMatch("/:coinId/chart");
+
     useEffect(() => {
         (async () => {
             const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
             const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
             setInfo(infoData);
             setPriceInfo(priceData);
+            setLoading(false);
         })();
-    }, []);
+    }, [coinId]);
     return (
         // state는 Coins 컴포넌트에서 넘어온 것
         // 따라서 Coins 컴포넌트에서 Link 를 클릭해서 넘어오지 않고 url을 직접 입력해서 Coin 컴포넌트가 렌더링된다면
@@ -134,9 +186,57 @@ function Coin() {
         // 예외 처리를 위한 ?를 붙여주자: state가 null이나 undefined가 아니면 state.name을 보여주고, 그렇지 않으면 "Loading"을 보여주라
         <Container>
             <Header>
-                <Title>{state?.name || "Loading"}</Title>
+                <Title>{state?.name ? state.name : loading ? "loading..." : info?.name}</Title>
             </Header>
-            {loading ? <Loader>Loading...</Loader> : null}
+            {loading ? (
+                <Loader>Loading...</Loader>
+            ) : (
+                <>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Rank:</span>
+                            <span>{info?.rank}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Symbol:</span>
+                            <span>{info?.symbol}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Open Source:</span>
+                            <span>{info?.open_source ? "Yes" : "No"}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Description>{info?.description}</Description>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Total Supply:</span>
+                            <span>{priceInfo?.total_supply}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Max Supply:</span>
+                            <span>{priceInfo?.max_supply}</span>
+                        </OverviewItem>
+                    </Overview>
+                    {/* 한번에 한 컴포넌트만 렌더링되길 바라니까 Switch 사용 */}
+                    <Tabs>
+                        <Tab isActive={priceMatch !== null}>
+                            <Link to={`/${coinId}/price`}>Price</Link>
+                        </Tab>
+                        <Tab isActive={chartMatch !== null}>
+                            <Link to={`/${coinId}/chart`}>Chart</Link>
+                        </Tab>
+                    </Tabs>
+
+                    <Switch>
+                        <Route path={`/:coinId/price`}>
+                            <Price />
+                        </Route>
+                        <Route path={`/:coinId/chart`}>
+                            <Chart />
+                        </Route>
+                    </Switch>
+                </>
+            )}
         </Container>
     );
 }
